08faeda0c5f5039560f8c8175dc88fb9f9a95c3a
Estelson Medeiros Pereira <estelson@gmail.com>
2025-06-13 20:44:07 -0300

    Creating a Event Detail Page UI from Scratch

--- lib/pages/detail_page.dart
+++ lib/pages/detail_page.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class DetailPage extends StatefulWidget {
@@ -12,30 +11,246 @@ class _DetailPageState extends State<DetailPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Column(
-          children: [
-            Stack(
-              children: [
-                Image.asset(
-                  "assets/images/event.jpg",
-                  width: MediaQuery.of(context).size.width,
-                  height: MediaQuery.of(context).size.height / 2.5,
-                  fit: BoxFit.cover,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Stack(
+            children: [
+              /// Event image
+              Image.asset(
+                "assets/images/event.jpg",
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height / 2,
+                fit: BoxFit.cover,
+              ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height / 2,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    /// Back button
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.pop(context);
+                      },
+                      child: Container(
+                        padding: const EdgeInsets.all(8),
+                        margin: const EdgeInsets.only(top: 40, left: 20),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                        child: const Icon(
+                          Icons.arrow_back_ios_new_outlined,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+
+                    /// Event info
+                    Container(
+                      padding: const EdgeInsets.only(left: 20),
+                      width: MediaQuery.of(context).size.width,
+                      decoration: const BoxDecoration(
+                        color: Colors.black45,
+                      ),
+                      child: const Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          /// Event title
+                          Text(
+                            "Dua Lipa Concert",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 25,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+
+                          /// Event date
+                          Row(
+                            children: [
+                              Icon(
+                                Icons.calendar_month,
+                                color: Colors.white,
+                              ),
+                              SizedBox(width: 10),
+                              Text(
+                                "31 Dec 2024",
+                                style: TextStyle(
+                                  color: Color.fromARGB(211, 255, 255, 255),
+                                  fontSize: 19,
+                                ),
+                              ),
+
+                              SizedBox(width: 20),
+
+                              /// Event location
+                              Icon(
+                                Icons.location_on_outlined,
+                                color: Colors.white,
+                              ),
+                              SizedBox(width: 10),
+                              Text(
+                                overflow: TextOverflow.ellipsis,
+                                "Mumbai, India",
+                                style: TextStyle(
+                                  color: Color.fromARGB(211, 255, 255, 255),
+                                  fontSize: 19,
+                                ),
+                              ),
+                            ],
+                          ),
+
+                          SizedBox(height: 20),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
-                Container(
-                  padding: EdgeInsets.all(8),
-                  margin: const EdgeInsets.only(top: 40, left: 20),
-                  decoration: BoxDecoration(color: Colors.white, borderRadius: BorderRadius.circular(30)),
-                  child: Icon(
-                    CupertinoIcons.back,
+              ),
+            ],
+          ),
+
+          const SizedBox(height: 20),
+
+          /// About event title
+          const Padding(
+            padding: EdgeInsets.only(left: 20),
+            child: Text(
+              "About event",
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+
+          const SizedBox(height: 10),
+
+          /// Event description
+          const Padding(
+            padding: EdgeInsets.only(left: 20),
+            child: Text(
+              "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+              style: TextStyle(
+                color: Colors.black87,
+                fontSize: 18,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ),
+
+          /// Number of tickets
+          Padding(
+            padding: const EdgeInsets.only(left: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  "Number of tickets:",
+                  style: TextStyle(
                     color: Colors.black,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+
+                const SizedBox(width: 40),
+
+                /// Quantity container
+                Container(
+                  margin: const EdgeInsets.only(right: 20),
+                  width: 50,
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.black54,
+                      width: 2,
+                    ),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: const Column(
+                    children: [
+                      /// Increase button
+                      Text(
+                        "+",
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 25,
+                        ),
+                      ),
+
+                      /// Quantity
+                      Text(
+                        "3",
+                        style: TextStyle(
+                          color: Color(0xff6351ec),
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+
+                      /// Decrease button
+                      Text(
+                        "-",
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 25,
+                        ),
+                      ),
+                    ],
                   ),
                 ),
               ],
             ),
-          ],
-        ),
+          ),
+
+          const SizedBox(height: 20),
+
+          /// Amount and book button session
+          Padding(
+            padding: const EdgeInsets.only(left: 10, right: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                /// Amount
+                const Text(
+                  "Amount: \$150.0",
+                  style: TextStyle(
+                    color: Color(0xff6351ec),
+                    fontSize: 23,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+
+                const SizedBox(width: 20),
+
+                /// Book button
+                Container(
+                  width: 170,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    color: const Color(0xff6351ec),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: const Center(
+                    child: Text(
+                      "Book now",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
